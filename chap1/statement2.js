f2b2fd2c492d7c6ee9fa3885acd7135ec10a7666
seungryeol <opop0421@naver.com>
2024-07-03 17:27:28 +0900

    statement()함수 반복문 쪼개기

--- chap1/statement2.js
+++ chap1/statement2.js
@@ -5,18 +5,21 @@ export function statement(invoice, plays) {
     let volumeCredits = 0;
     let result = `청구내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
-
-        volumeCredits += volumeCreditsFor(perf)
-
         // 청구 내역을 출력한다.
         result += `${play.name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
         totalAmount += amountFor(perf);
     }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
+    }
+
     result += `총액 ${usd(totalAmount)}\n`;
     result += `적립 포인트 ${volumeCredits}점\n`;
   
     return result;
 }
+
+
 
 function usd(aNumber){
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(aNumber/100);
