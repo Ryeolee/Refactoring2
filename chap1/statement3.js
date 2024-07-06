9e2c187827bae3010b27ee7da075f0f570c5d1d6
seungryeol <opop0421@naver.com>
2024-07-06 12:57:05 +0900

    enrichPerformance() 함수 추가

--- chap1/statement3.js
+++ chap1/statement3.js
@@ -5,97 +5,80 @@ const play = require('../plays.json');
 
 
 /**
- * ## 리팩토링 순서
- * 1. 반복문 쪼개기
- * 2. 문장 슬라이드하기
- * 3. 함수 추출하기
+ * ## 계산 관련 코드 모음
  * 4. 변수 인라인하기
  * @param {*} invoice 
  * @param {*} plays 
  * @returns 
  */
 function statement(invoice, plays) {
-    return renderPainText(invoice, plays)
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return renderPainText(statementData, plays)
 
-
-    function renderPainText(invoice, plays){
-        let result = `청구내역 (고객명: ${invoice.customer})\n`;
-        for (let perf of invoice.performances) {
-            // 청구 내역을 출력한다.
-            result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
-        }
-     
-        result += `총액 ${usd(totalAmount())}\n`;
-        result += `적립 포인트 ${totalVolumeCedits()}점\n`;
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+    
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
-    }
+      }
     
-    
-    function totalAmount(){
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-    
-        return result;
-    
-    }
-    
-    function totalVolumeCedits(){
-        let volumeCredits = 0;
-        for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf)
-        }
-        return volumeCredits;
-    }
-    
-    
-    
-    function usd(aNumber){
-        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(aNumber/100);
-    }
-    
-    
-    function volumeCreditsFor(perf){
-          let result = 0;
-          result += Math.max(perf.audience - 30, 0);
-      
-          // 희극 관객 5명마다 추가 포인트를 제공한다.
-          if ('comedy' === play.type) {
-            result += Math.floor(perf.audience / 5);
-          }
-          return result;
-    }
-    
-    function playFor(aPerformance) {
+      function playFor(aPerformance) {
         return plays[aPerformance.playID];
-        }
+      }
     
-    function amountFor(aPerformance) {
+      function amountFor(aPerformance) {
         let result = 0;
     
-        switch (playFor(aPerformance).type) {
-            case 'tragedy':
+        switch (aPerformance.play.type) {
+          case 'tragedy':
             result = 40_000;
     
             if (aPerformance.audience > 30) {
-                result += 1_000 * (aPerformance.audience - 30);
+              result += 1_000 * (aPerformance.audience - 30);
             }
             break;
-            case 'comedy':
+          case 'comedy':
             result = 30_000;
     
             if (aPerformance.audience > 20) {
-                result += 10_000 + 500 * (aPerformance.audience - 20);
+              result += 10_000 + 500 * (aPerformance.audience - 20);
             }
             result += 300 * aPerformance.audience;
             break;
     
-            default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+          default:
+            throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
         return result;
+      }
+    
+      function volumeCreditsFor(aPerformance) {
+        let result = 0;
+    
+        result += Math.max(aPerformance.audience - 30, 0);
+    
+        if ('comedy' === aPerformance.play.type) {
+          result += Math.floor(aPerformance.audience / 5);
         }
+    
+        return result;
+      }
+    
+      function totalAmount(data) {
+        return data.performances.reduce((acc, cur) => acc + cur.amount, 0);
+      }
+    
+      function totalVolumeCredits(data) {
+        return data.performances.reduce((acc, cur) => acc + cur.volumeCredits, 0);
+      }
+    
+
 }
 
 
