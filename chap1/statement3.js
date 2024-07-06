e08aa7aa2f209333bb06630e186fcb099d7716c1
seungryeol <opop0421@naver.com>
2024-07-06 13:20:53 +0900

    html 함수 추가

--- chap1/statement3.js
+++ chap1/statement3.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
 
 function renderPlainText(data) {
   let result = `청구내역 (고객명: ${data.customer})\n`;
+  console.log(result);
 
   for (let perf of data.performances) {
     result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
@@ -19,9 +20,33 @@ function renderPlainText(data) {
   result += `총액 ${usd(data.totalAmount)}\n`;
   result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
 
+  console.log(result)
+
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+  }
+  
+  function renderHtml(data) {
+    let result = `<h1>청구 내역 ${data.customer}</h1>\n`;
+  
+    result += '<table>\n';
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+  
+    for (let perf of data.performances) {
+      result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}석</td>`;
+      result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  
+    return result;
+  }
+
+  
 function usd(aNumber) {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(
       aNumber / 100
