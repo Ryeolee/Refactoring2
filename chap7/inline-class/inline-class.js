15529dabfe8246f01a33df55fe0d51cfb59894d9
seungryeol <opop0421@naver.com>
2024-07-23 03:35:20 +0900

    클래스 인라인 하기

---
+++ chap7/inline-class/inline-class.js
@@ -0,0 +1,29 @@
+
+// 해당 클래스에 함수 합치기
+class Shipment {
+
+    get trackingInfo() {
+      return `${this.shippingCompany}: ${this.trackingNumber}`;
+    }
+    get trackingInformation() {
+      return this._trackingInformation;
+    }
+    set trackingInformation(aTrackingInformation) {
+      this._trackingInformation = aTrackingInformation;
+    }
+    get shippingCompany() {
+      return this._shippingCompany;
+    }
+    set shippingCompany(arg) {
+      this._shippingCompany = arg;
+    }
+    get trackingNumber() {
+      return this._trackingNumber;
+    }
+    set trackingNumber(arg) {
+      this._trackingNumber = arg;
+    }
+  }
+  
+  // 클라이언트
+  aShipment.shippingCompany = request.vendor;
